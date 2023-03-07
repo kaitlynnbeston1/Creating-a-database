106613d87125960065af293b40d537103726f426
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2023-03-07 09:16:10 -0600

    Fixed tiny mistake (I FORGOT THE COMMAS!!!)

--- thanos_database.py
+++ thanos_database.py
@@ -1,8 +1,8 @@
 # Yifei, Kaitlynn
 thanos = {
-    "name": "dione"
-    "superpowers": ["superhuman strength", "speed", "stamina", "imortality", "invulnerability", "telekinesis", "telepathy"]
-    "weaknesses": ["his own monstrous ego", "losing infinity gontlet"]
-    "weapon": "infinity gontlet"
+    "name": "dione",
+    "superpowers": ["superhuman strength", "speed", "stamina", "imortality", "invulnerability", "telekinesis", "telepathy"],
+    "weaknesses": ["his own monstrous ego", "losing infinity gontlet"],
+    "weapon": "infinity gontlet",
     }
 
