8aef403ccaf81680f341828c5934b2031becef29
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2023-03-07 09:07:58 -0600

    Init

---
+++ thanos_database.py
@@ -0,0 +1,8 @@
+# Yifei, Kaitlynn
+thanos = {
+    "name": "dione"
+    "superpowers": ["superhuman strength", "speed", "stamina", "imortality", "invulnerability", "telekinesis", "telepathy"]
+    "weaknesses": ["his own monstrous ego", "losing infinity gontlet"]
+    "weapon": "infinity gontlet"
+    }
+
